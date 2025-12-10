9d20bed3fd54be99dd1d74ad2cec5851b29125a1
Alex Varghese <117454288+Alex-Varghese@users.noreply.github.com>
2025-12-10 16:04:06 +0530

    Remove commented line for clarity

--- src/globals/apb_global_pkg.sv
+++ src/globals/apb_global_pkg.sv
@@ -1,7 +1,7 @@
 `ifndef APB_GLOBAL_PKG_INCLUDED_
 `define APB_GLOBAL_PKG_INCLUDED_
 
-// get val
+
 
 //--------------------------------------------------------------------------------------------
 // Package : apb_global_pkg
